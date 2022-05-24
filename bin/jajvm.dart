6ffd1a6ea733927abbb8ce9b5f10c70bef1e2759
getBoolean <danielwnewville@gmail.com>
2022-05-23 19:20:25 -0500

    Initial commit

---
+++ bin/jajvm.dart
@@ -0,0 +1,5 @@
+import 'package:jajvm/jajvm.dart' as jajvm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${jajvm.calculate()}!');
+}
---
+++ test/jajvm_test.dart
@@ -0,0 +1,8 @@
+import 'package:jajvm/jajvm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
